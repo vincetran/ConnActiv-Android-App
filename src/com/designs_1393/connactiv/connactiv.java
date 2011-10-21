b51be332d543ceda0bee4fb91b4bd3e795b79648
Vince Tran <hello@vincetran.me>
2011-10-20 22:41:29 -0400

    First commit; barebones android app

---
+++ src/com/designs_1393/connactiv/connactiv.java
@@ -0,0 +1,15 @@
+package com.designs_1393.connactiv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class connactiv extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
