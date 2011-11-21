1c807f83512ae6f6aedf79d8667d0e0e67b1711e
sjbarag <sjbarag@gmail.com>
2011-11-21 16:50:25 -0500

    Add actionbar and switch to ListActivity

--- src/com/designs_1393/connactiv/connactiv.java
+++ src/com/designs_1393/connactiv/connactiv.java
@@ -2,14 +2,24 @@ package com.designs_1393.connactiv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.ListActivity;
 
-public class connactiv extends Activity
+// actionbar
+import com.markupartist.android.widget.ActionBar;
+import com.markupartist.android.widget.ActionBar.Action;
+import com.markupartist.android.widget.ActionBar.IntentAction;
+
+public class connactiv extends ListActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.stream);
+
+		// ----- actionbar -----
+		ActionBar actionBar = (ActionBar) findViewById(R.id.actionbar);
+		actionBar.setTitle("ConnActiv Stream");
     }
 }
